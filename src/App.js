3c0cf5fdd6b39fd11f4d4dfb5b20f5b383936be3
bryanmmr <bryanmmr@gmail.com>
2021-11-25 19:57:44 -0500

    add: increase number

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {number: 0}
+  handleClick = () =>{
+    this.setState({number : this.state.number + 1})
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
-      </div>
+          <span className="value">{this.state.number}</span>
+          <button id="inc" onClick={this.handleClick}>Incrementa</button>
+      </div>    
     );
   }
 }
